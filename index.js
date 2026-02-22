db21b40a84b6c4940f6116bfe61a525c0f4f4ad5
kumarsuraj345678 <kumarsuraj345678@gmail.com>
2026-02-22 14:50:12 +0530

    use a constant for the server port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 
+const PORT = process.env.PORT || 5000
+
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 
@@ -22,6 +24,6 @@ app.use("/api/books", bookRoutes);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
