4415f7b8d809b692ab192cc777f39b004a3fd9cd
kumarsuraj345678 <kumarsuraj345678@gmail.com>
2026-02-22 14:51:00 +0530

    update server log to use the defined PORT constant

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use("/api/books", bookRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
